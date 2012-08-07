0f9e102dadc87b45a6810d82aa394dca5fccc3f1
Grayson Koonce <grayson.koonce@gmail.com>
2012-08-07 13:38:48 -0700

    insertion sort

--- src/Algorithm/Sort/InsertionSort.php
+++ src/Algorithm/Sort/InsertionSort.php
@@ -2,7 +2,35 @@
 
 namespace Algorithm\Sort;
 
+/**
+ * @see http://www.stoimen.com/blog/2012/02/13/computer-algorithms-insertion-sort/
+ */
 class InsertionSort
 {
-	
+    public function sort(array &$array)
+    {
+        // loop through all items in the array
+        for ($i = 1; $i < count($array); $i++) {
+
+            // store current value in temp
+            $temp = $array[$i];
+
+            // store counter in temp
+            $j = $i;
+
+            // while temp counter is greater than or equal to 0,
+            // and the previous value in the array is greater than temp
+            while (($j >= 0) && ($array[$j-1] > $temp)) {
+
+                // move the previous value forward one
+                $array[$j] = $array[$j - 1];
+
+                // decrement the temp counter
+                $j--;
+            }
+
+            // move the current value backwards
+            $array[$j] = $temp;
+        }
+    }
 }
--- tests/Algorithm/Tests/Sort/InsertionSortTest.php
+++ tests/Algorithm/Tests/Sort/InsertionSortTest.php
@@ -2,10 +2,29 @@
 
 namespace Algorithm\Tests\Sort;
 
+use Algorithm\Sort\InsertionSort;
+
 class InsertionSortTest extends \PHPUnit_Framework_TestCase
 {
-    public function testShouldBeTrue()
+    public function setUp()
     {
-        $this->assertTrue(true);
+        $this->subject = new InsertionSort();
+    }
+
+    /**
+     * @todo add timing mechanism
+     */
+    public function testShouldSort()
+    {
+        $array = array(1,5,3,19,3,6,11);
+        
+        // copy array and use PHP to sort
+        $sorted = $array;
+        sort($sorted);
+
+        // sort the array using the subject
+        $this->subject->sort($array);
+
+        $this->assertEquals($array, $sorted);
     }
 }
