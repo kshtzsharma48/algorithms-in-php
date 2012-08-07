741a025f8be3f0cb9ef6da28a200734f67240f72
Grayson Koonce <grayson.koonce@gmail.com>
2012-08-07 13:22:58 -0700

    initial commit

---
+++ src/Algorithm/Sort/InsertionSort.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Algorithm\Sort;
+
+class InsertionSort
+{
+	
+}
---
+++ tests/Algorithm/Tests/Sort/InsertionSortTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Algorithm\Tests\Sort;
+
+class InsertionSortTest extends \PHPUnit_Framework_TestCase
+{
+	public function testShouldBeTrue()
+	{
+		$this->assertTrue(true);
+	}
+}
