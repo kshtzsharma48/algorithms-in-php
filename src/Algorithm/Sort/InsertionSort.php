ee98e3b023ef2f697f1fb1003a89be9aa7665505
Grayson Koonce <grayson.koonce@gmail.com>
2012-08-08 01:09:45 -0700

    simplifying insertion sort algorithm

--- src/Algorithm/Sort/InsertionSort.php
+++ src/Algorithm/Sort/InsertionSort.php
@@ -9,54 +9,23 @@ namespace Algorithm\Sort;
 class InsertionSort
 {
     /**
-     * manually sort an array [8, 2, 4, 9, 3, 6]
-     *
      * 8 2 4 9 3 6 - start on second position, 2 moves left one
      * 2 8 4 9 3 6 - third position, 4 moves left one
      * 2 4 8 9 3 6 - fourth position, 9 does not move
      * 2 4 8 9 3 6 - fifth position, 3 moves left four
      * 2 3 4 8 9 6 - sixth position, 6 moves left three
      * 2 3 4 6 8 9 - done
-     *
-     * Running time:
-     *  - depends on the input (e.g. already sorted, worst case reverse sorted)
-     *  - depends on the input size (6 elements vs 60000 elements)
-     *  - want upper bounds on the running time (e.g. won't run longer than 3 seconds)
-     *  - upper bounds represents a gaurantee to the user
-     *
-     * Kinds of analysis:
-     *  - worst case (most popular)
-     *      - T(n) = max time of any input of size n
-     *  - average case
-     *      - T(n) = expected time over all inputs of size n
-     *      - this requires an assumption of the statistical distribution of inputs
-     *  - best case
-     *      - this is bogus because you can make slow algorithms look fast (cheating)
-     *      - a slow algorithm can look fast on some inputs (cheating)
      */
     public function sort(array &$array)
     {
-        // loop through all items in the array
         for ($i = 1; $i < count($array); $i++) {
-
-            // store current value in temp
             $current = $array[$i];
-
-            // store counter in temp
             $j = $i;
 
-            // while temp counter is greater than or equal to 0,
-            // and the previous value in the array is greater than temp
-            while (($j >= 0) && ($array[$j-1] > $current)) {
-
-                // move the previous value forward one
+            while (($j >= 0) && ($array[$j - 1] > $current)) {
                 $array[$j] = $array[$j - 1];
-
-                // decrement the temp counter
                 $j--;
             }
-
-            // move the current value backwards
             $array[$j] = $current;
         }
     }
